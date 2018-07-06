2fc179096e20d4ef4f0711cf971eaa3c84e6dcd2
Shubhank Tyagi <shubhank.tyagi@gmail.com>
2018-07-06 16:48:59 +0530

    updated DoorLock.ino
    added passcode verification. But still has some unwanted outputs. Might infact remove the keypad functionality.

--- DoorLock.ino
+++ DoorLock.ino
@@ -19,6 +19,8 @@ char keymap[Rows][Cols] =
 
 byte rPins[Rows] = {8,7,6,5};
 byte cPins[Cols] = {4,3,2,A3};
+String pw = "123A";
+String keypressed = "";
 Keypad kpd = Keypad(makeKeymap(keymap), rPins, cPins, Rows, Cols);
 //MFRC522 rc522(SS_PIN, RST_PIN);
 
@@ -26,12 +28,35 @@ void setup() {
   // put your setup code here, to run once:
   //rc522.PCD_Init();     //Initialize card reader
   Serial.begin(9600);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
   Serial.println("Enter the CODE:");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  char keypressed = kpd.getKey();
-  if(keypressed != NO_KEY)
-    Serial.println(keypressed);
+  char key = kpd.getKey();
+  if(key!=NO_KEY){
+  Serial.print(key);
+  keypressed.concat(key);
+  if(keypressed == pw){
+    digitalWrite(A0, HIGH);
+    tone(Buzz, 1000,500);
+    Serial.println("\nWelcome Shubhank");
+    delay(500);
+    digitalWrite(A0, LOW);
+    keypressed = "";
+    }
+  else{
+    digitalWrite(A1, HIGH);
+    tone(Buzz, 300, 200);
+    Serial.println("\nWRONG!");
+    delay(250);
+    tone(Buzz, 300, 200);
+    delay(250);
+    tone(Buzz, 300, 200);
+    digitalWrite(A1, LOW);
+    }
+  }
 }
